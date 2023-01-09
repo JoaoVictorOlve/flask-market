cbd9af85e677aac3404f6b4c34a0e484009ef55c
João Victor <jvoliveira2005@gmail.com>
2023-01-09 09:02:44 -0300

    Adicionando pasta static, arquivo css e corrigindo aspas

--- market/__init__.py
+++ market/__init__.py
@@ -6,12 +6,12 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.app_context().push()
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-app.config['SECRET_KEY'] = 'dfbb233fe4b817c1070d47c9'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
+app.config["SECRET_KEY"] = "dfbb233fe4b817c1070d47c9"
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login_page'
-login_manager.login_message_category = 'info'
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 
 from market import routes
